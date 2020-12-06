552c74a429d6f967780fc3989263dd4d86146fb8
MaHST0621 <1186217640@qq.com>
2020-12-06 23:43:59 +0000

    1.打包类结构和初始函数完成

--- UDP-pro/server.cpp
+++ UDP-pro/server.cpp
@@ -8,7 +8,59 @@
 #include <string.h>
 
 #define SERV_PORT   8000   
+#define SEND_BUFF   256
   
+
+
+u_short cksum(u_short *buf,int count)
+{
+    u_long sum = 0;
+    while(count--)
+    {
+        sum += *buf++;
+        if(sum & 0xFFFF0000)
+        {
+            sum &= 0xFFFF;
+            sum++;
+        }
+    }
+    return ~(sum & 0xFFFF);
+}
+
+
+
+class Rdt
+{
+public:
+    int ack_id;
+    int ack = 1;
+    char* Send_buff = new char[SEND_BUFF];
+
+public:
+    void make_pak(int id,char* buf);
+    int  getlen_buf(char* buf);
+
+};
+
+int Rdt::getlen_buf(char* buf)
+{
+    return strlen(buf);
+}
+
+void Rdt::make_pak(int id,char* buf)
+{
+    if(id == 1){
+        Send_buff[0] |= 0x1;
+    }
+    else if (id == 0){
+        Send_buff[0] |= 0x0;
+    }
+
+    Send_buff[1] = cksum((u_short*)buf,getlen_buf(buf));
+    Send_buff[3] = *buf;
+}
+
+
 int main()  
 {  
   /* sock_fd --- socket文件描述符 创建udp套接字*/  
@@ -19,12 +71,12 @@ int main()
         exit(1);  
     }  
   
-  /* 将套接字和IP、端口绑定 */  
     struct sockaddr_in addr_serv;  
     int len;  
-    memset(&addr_serv, 0, sizeof(struct sockaddr_in));  //每个字节都用0填充
-    addr_serv.sin_family = AF_INET;  　　　　　　　　　　　 //使用IPV4地址
-    addr_serv.sin_port = htons(SERV_PORT);  　　　　　　　 //端口
+    /*每个字节都用0填充*/
+    memset(&addr_serv, 0, sizeof(struct sockaddr_in)); 
+    addr_serv.sin_family = AF_INET;
+    addr_serv.sin_port = htons(SERV_PORT); 
     /* INADDR_ANY表示不管是哪个网卡接收到数据，只要目的端口是SERV_PORT，就会被该应用程序接收到 */  
     addr_serv.sin_addr.s_addr = htonl(INADDR_ANY);  //自动获取IP地址
     len = sizeof(addr_serv);  
@@ -39,31 +91,33 @@ int main()
     
     int recv_num;  
     int send_num;  
-    char send_buff[20] = "i am server!";
+    char send_buff[SEND_BUFF] = "i am server!";
     char recv_buff[20];
 
     struct sockaddr_in addr_client;  
-      
+
+    Rdt Rdt_udp;
+    Rdt_udp.make_pak(0,send_buff);  
     while(1)  
         {  
-            printf("server wait:\n");  
+            printf("等待链接:\n");  
                   
             recv_num = recvfrom(sock_fd, recv_buff, sizeof(recv_buff), 0, (struct sockaddr *)&addr_client, (socklen_t *)&len);  
                   
             if(recv_num < 0)  
                 {  
-                    perror("recvfrom error:");  
+                    perror("接受报错:");  
                     exit(1);  
                 }  
               
             recv_buff[recv_num] = '\0';
             printf("server receive %d bytes: %s\n", recv_num, recv_buff);  
               
-            send_num = sendto(sock_fd, send_buff, recv_num, 0, (struct sockaddr *)&addr_client, len);  
+            send_num = sendto(sock_fd, Rdt_udp.Send_buff, recv_num, 0, (struct sockaddr *)&addr_client, len);  
                   
             if(send_num < 0)  
                 {  
-                    perror("sendto error:");  
+                    perror("发送报错:");  
                     exit(1);  
                 }  
         }  
